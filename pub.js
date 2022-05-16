2bbe1dcd493783868ae32e788ed7861d08de8ea9
Fabio Luis Dumont <fabioluisdumont@gmail.com>
2022-05-16 09:54:42 -0300

    MQTT inicial
    Para rodar iniciar broker(node broker), iniciar sub(node sub), iniciar pub(node pub).

---
+++ pub.js
@@ -0,0 +1,11 @@
+// MQTT publisher
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost:1234')
+var topic = 'LINTANGtest123'
+var message = 'Hello World!'
+
+client.on('connect', ()=>{
+        client.publish(topic, message)
+        console.log('Message sent!', message)
+    }
+)
