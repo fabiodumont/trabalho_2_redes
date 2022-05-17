76b15758e253310b44d1dd2ff43c8f907f5bebd7
Fabio Luis Dumont <fabioluisdumont@gmail.com>
2022-05-16 23:54:12 -0300

    Broker atualizado

--- broker.js
+++ broker.js
@@ -1,13 +1,10 @@
-// Mosca MQTT broker
-var mosca = require('mosca')
-var settings = {port: 1234}
-var broker = new mosca.Server(settings)
-
-broker.on('ready', ()=>{
-    console.log('Broker is ready!')
-})
-
-broker.on('published', (packet)=>{
-    message = packet.payload.toString()
-    console.log(message)
-})
+var mosca = require('mosca');
+var settings = {
+        port:1883
+        }
+ 
+var server = new mosca.Server(settings);
+ 
+server.on('start', function(){
+console.log("start");
+});
